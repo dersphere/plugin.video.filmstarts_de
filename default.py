6ef913128cecf59a02c38018f6b520f4c004b91c
dersphere <sphere@dersphere.de>
2013-03-29 16:53:45 +0100

    fix E231: missing whitespace after ","

--- default.py
+++ default.py
@@ -25,24 +25,24 @@ viewMode = str(addon.getSetting("viewMode"))
 
 
 def index():
-    addDir('Trailer: ' + translation(30008),'',"search",'')
-    addDir('Trailer: ' + translation(30001),'http://www.filmstarts.de/trailer/aktuell_im_kino.html?version=1',"showSortDirection",'')
-    addDir('Trailer: ' + translation(30002),'http://www.filmstarts.de/trailer/bald_im_kino.html?version=1',"showSortDirection",'')
-    addDir('Filmstarts: Interviews','http://www.filmstarts.de/trailer/interviews/',"listVideosInterview",'')
-    addDir('Filmstarts: Fünf Sterne','http://www.filmstarts.de/videos/shows/funf-sterne',"listVideosMagazin",'')
-    addDir('Filmstarts: Fehlerteufel','http://www.filmstarts.de/videos/shows/filmstarts-fehlerteufel',"listVideosMagazin",'')
-    addDir('Meine Lieblings-Filmszene','http://www.filmstarts.de/videos/shows/meine-lieblings-filmszene',"listVideosMagazin",'')
-    addDir('Serien-Trailer','http://www.filmstarts.de/trailer/serien/',"listVideosTV",'')
+    addDir('Trailer: ' + translation(30008), '', "search", '')
+    addDir('Trailer: ' + translation(30001), 'http://www.filmstarts.de/trailer/aktuell_im_kino.html?version=1', "showSortDirection", '')
+    addDir('Trailer: ' + translation(30002), 'http://www.filmstarts.de/trailer/bald_im_kino.html?version=1', "showSortDirection", '')
+    addDir('Filmstarts: Interviews', 'http://www.filmstarts.de/trailer/interviews/', "listVideosInterview", '')
+    addDir('Filmstarts: Fünf Sterne', 'http://www.filmstarts.de/videos/shows/funf-sterne', "listVideosMagazin", '')
+    addDir('Filmstarts: Fehlerteufel', 'http://www.filmstarts.de/videos/shows/filmstarts-fehlerteufel', "listVideosMagazin", '')
+    addDir('Meine Lieblings-Filmszene', 'http://www.filmstarts.de/videos/shows/meine-lieblings-filmszene', "listVideosMagazin", '')
+    addDir('Serien-Trailer', 'http://www.filmstarts.de/trailer/serien/', "listVideosTV", '')
     xbmcplugin.endOfDirectory(pluginhandle)
     if forceViewMode == True:
         xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
 
 
 def showSortDirection(url):
-    addDir(translation(30003),url.replace("?version=1","?sort_order=0&version=1"),"listVideosTrailer",'')
-    addDir(translation(30004),url.replace("?version=1","?sort_order=1&version=1"),"listVideosTrailer",'')
-    addDir(translation(30005),url.replace("?version=1","?sort_order=3&version=1"),"listVideosTrailer",'')
-    addDir(translation(30006),url.replace("?version=1","?sort_order=2&version=1"),"listVideosTrailer",'')
+    addDir(translation(30003), url.replace("?version=1", "?sort_order=0&version=1"), "listVideosTrailer", '')
+    addDir(translation(30004), url.replace("?version=1", "?sort_order=1&version=1"), "listVideosTrailer", '')
+    addDir(translation(30005), url.replace("?version=1", "?sort_order=3&version=1"), "listVideosTrailer", '')
+    addDir(translation(30006), url.replace("?version=1", "?sort_order=2&version=1"), "listVideosTrailer", '')
     xbmcplugin.endOfDirectory(pluginhandle)
     if forceViewMode == True:
         xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
@@ -65,27 +65,27 @@ def listVideos(urlFull):
             pass
     if mode == "listVideosTrailer":
         match = re.compile('<img src=\'(.+?)\' alt="(.+?)" title="(.+?)" />\n</span>\n</div>\n<div class="contenzone">\n<div class="titlebar">\n<a class="link" href="(.+?)">\n<span class=\'bold\'>(.+?)</span>', re.DOTALL).findall(content)
-        for thumb,temp1,temp2,url,title in match:
+        for thumb, temp1, temp2, url, title in match:
             if showAllTrailers == "true":
                 url = url[:url.find("/trailer/")] + "/trailers/"
-                addDir(title,'http://www.filmstarts.de' + url,"listTrailers",get_better_thumb(thumb))
+                addDir(title, 'http://www.filmstarts.de' + url, "listTrailers", get_better_thumb(thumb))
             else:
-                addLink(title,'http://www.filmstarts.de' + url,"playVideo",get_better_thumb(thumb))
+                addLink(title, 'http://www.filmstarts.de' + url, "playVideo", get_better_thumb(thumb))
     elif mode == "listVideosMagazin":
         if currentPage == 1:
             match = re.compile('<a href="(.+?)">\n<img src="(.+?)" alt="" />\n</a>\n</div>\n<div style="(.+?)">\n<h2 class="(.+?)" style="(.+?)"><b>(.+?)</b> (.+?)</h2><br />\n<span style="(.+?)" class="purehtml fs11">\n(.+?)<a class="btn" href="(.+?)"', re.DOTALL).findall(content)
-            for temp0,thumb,temp1,temp2,temp3,temp4,title,temp5,temp6,url in match:
-                addLink(title,'http://www.filmstarts.de' + url,"playVideo",get_better_thumb(thumb))
+            for temp0, thumb, temp1, temp2, temp3, temp4, title, temp5, temp6, url in match:
+                addLink(title, 'http://www.filmstarts.de' + url, "playVideo", get_better_thumb(thumb))
         match = re.compile('<img src=\'(.+?)\' alt="(.+?)" title="(.+?)" />\n</span>\n</div>\n<div class="contenzone">\n<div class="titlebar">\n<a href=\'(.+?)\' class="link">\n<span class=\'bold\'><b>(.+?)</b> (.+?)</span>', re.DOTALL).findall(content)
-        for thumb,temp1,temp2,url,temp3,title in match:
-            addLink(title,'http://www.filmstarts.de' + url,"playVideo",get_better_thumb(thumb))
+        for thumb, temp1, temp2, url, temp3, title in match:
+            addLink(title, 'http://www.filmstarts.de' + url, "playVideo", get_better_thumb(thumb))
     elif mode == "listVideosInterview":
         match = re.compile('<img src=\'(.+?)\'(.+?)</span>\n</div>\n<div class="contenzone">\n<div class="titlebar">\n<a(.+?)href=\'(.+?)\'>\n<span class=\'bold\'>\n(.+?)\n</span>(.+?)\n</a>', re.DOTALL).findall(content)
-        for thumb,temp1,temp2,url,title1,title2 in match:
-            addLink(title1 + title2,'http://www.filmstarts.de' + url,"playVideo",get_better_thumb(thumb))
+        for thumb, temp1, temp2, url, title1, title2 in match:
+            addLink(title1 + title2, 'http://www.filmstarts.de' + url, "playVideo", get_better_thumb(thumb))
     elif mode == "listVideosTV":
         spl = content.split('<div class="datablock vpadding10b">')
-        for i in range(1,len(spl),1):
+        for i in range(1, len(spl), 1):
             entry = spl[i]
             match = re.compile("<a href='(.+?)'>", re.DOTALL).findall(entry)
             url = match[0]
@@ -97,7 +97,7 @@ def listVideos(urlFull):
             else:
                 match = re.compile("<a href='(.+?)'>\n(.+?)<br />", re.DOTALL).findall(entry)
                 title = match[0][1]
-            addLink(title,'http://www.filmstarts.de' + url,"playVideo",get_better_thumb(thumb))
+            addLink(title, 'http://www.filmstarts.de' + url, "playVideo", get_better_thumb(thumb))
     if currentPage<maxPage:
         urlNew = ""
         if mode == "listVideosTrailer":
@@ -109,7 +109,7 @@ def listVideos(urlFull):
             urlNew = 'http://www.filmstarts.de/' + match[0][0] + 'page='+str(currentPage + 1)
         elif urlFull.find('?page=') == -1 and (mode == "listVideosMagazin" or mode == "listVideosInterview" or mode == "listVideosTV"):
             urlNew = urlFull + "?page=" + str(currentPage + 1)
-        addDir(translation(30007) + " (" + str(currentPage + 1) + ")",urlNew,mode,'')
+        addDir(translation(30007) + " (" + str(currentPage + 1) + ")", urlNew, mode, '')
     xbmcplugin.endOfDirectory(pluginhandle)
     if forceViewMode == True:
         xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
@@ -118,11 +118,11 @@ def listVideos(urlFull):
 def listTrailers(url):
     content = getUrl(url)
     splMain = content.split('<div class="media_list_02')
-    for i in range(1,len(splMain),1):
+    for i in range(1, len(splMain), 1):
         entryMain = splMain[i]
         entryMain = entryMain[:entryMain.find('</div>')]
         spl = entryMain.split("<li>")
-        for i in range(1,len(spl),1):
+        for i in range(1, len(spl), 1):
             entry = spl[i]
             match = re.compile("src='(.+?)'", re.DOTALL).findall(entry)
             thumb = match[0]
@@ -132,16 +132,16 @@ def listTrailers(url):
             match = re.compile('href="(.+?)"', re.DOTALL).findall(entry)
             if len(match) > 0:
                 url = "http://www.filmstarts.de"+match[0]
-                addLink(title,url,'playVideo',thumb)
+                addLink(title, url, 'playVideo', thumb)
     xbmcplugin.endOfDirectory(pluginhandle)
     if forceViewMode == True:
         xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
 
 
 def cleanTitle(title):
-    title = title.replace("&lt;","<").replace("&gt;",">").replace("&amp;","&").replace("&#39;","'").replace("&quot;","\"").replace("&szlig;","ß").replace("&ndash;","-")
-    title = title.replace("&#38;","&").replace("&#8230;","...").replace("&#8211;","-").replace("&#8220;","-").replace("&#8221;","-").replace("&#8217;","'")
-    title = title.replace("&#196;","Ä").replace("&#220;","Ü").replace("&#214;","Ö").replace("&#228;","ä").replace("&#252;","ü").replace("&#246;","ö").replace("&#223;","ß").replace("&#176;","°").replace("&#233;","é").replace("&#224;","à")
+    title = title.replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&").replace("&#39;", "'").replace("&quot;", "\"").replace("&szlig;", "ß").replace("&ndash;", "-")
+    title = title.replace("&#38;", "&").replace("&#8230;", "...").replace("&#8211;", "-").replace("&#8220;", "-").replace("&#8221;", "-").replace("&#8217;", "'")
+    title = title.replace("&#196;", "Ä").replace("&#220;", "Ü").replace("&#214;", "Ö").replace("&#228;", "ä").replace("&#252;", "ü").replace("&#246;", "ö").replace("&#223;", "ß").replace("&#176;", "°").replace("&#233;", "é").replace("&#224;", "à")
     title = title.strip()
     return title
 
@@ -150,19 +150,19 @@ def search():
     keyboard = xbmc.Keyboard('', str(translation(30008)))
     keyboard.doModal()
     if keyboard.isConfirmed() and keyboard.getText():
-        search_string = keyboard.getText().replace(" ","+")
+        search_string = keyboard.getText().replace(" ", "+")
         content = getUrl("http://www.filmstarts.de/suche/1/?q=" + search_string)
         spl = content.split('<tr><td style=" vertical-align:middle;">')
-        for i in range(1,len(spl),1):
+        for i in range(1, len(spl), 1):
             entry = spl[i]
             match = re.compile("src='(.+?)'", re.DOTALL).findall(entry)
             thumb = match[0]
             match = re.compile("'>\n(.+?)</a>", re.DOTALL).findall(entry)
-            title = match[0].replace("<b>","").replace("</b>","")
+            title = match[0].replace("<b>", "").replace("</b>", "")
             title = cleanTitle(title)
             match = re.compile("href='(.+?)'", re.DOTALL).findall(entry)
-            url = "http://www.filmstarts.de"+match[0].replace(".html","/trailers/")
-            addDir(title,url,'listTrailers',thumb)
+            url = "http://www.filmstarts.de"+match[0].replace(".html", "/trailers/")
+            addDir(title, url, 'listTrailers', thumb)
         xbmcplugin.endOfDirectory(pluginhandle)
         if forceViewMode == True:
             xbmc.executebuiltin('Container.SetViewMode(' + viewMode + ')')
@@ -241,22 +241,22 @@ def parameters_string_to_dict(parameters):
     return paramDict
 
 
-def addLink(name,url,mode,iconimage):
+def addLink(name, url, mode, iconimage):
     u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode)
     ok = True
     liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
     liz.setProperty('IsPlayable', 'true')
-    ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),url=u,listitem=liz)
+    ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url=u, listitem=liz)
     return ok
 
 
-def addDir(name,url,mode,iconimage):
+def addDir(name, url, mode, iconimage):
     u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode)
     ok = True
     liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
     liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+    ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
     return ok
 
 
